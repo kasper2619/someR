c9869561653cb12cb0276f36afb1c1c584fc8e45
kasper <kasper2304@gmail.com>
2022-02-23 17:58:24 +0000

    - update

--- projects/folketinget/job_scheduler.R
+++ projects/folketinget/job_scheduler.R
@@ -5,3 +5,5 @@ cmd <- cron_rscript("/home/kasper/someR/projects/folketinget/engine/get_twitter_
 cron_add(cmd, frequency = 'daily', id = 'job10', at = '23:59', days_of_week = c(1,2,3,4,5,6,7))
 cron_ls()
 cron_clear()
+
+# this can be
